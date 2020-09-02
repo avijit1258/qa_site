b59c48202afcd3bef372b54b38159c425279372e
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2020-09-02 16:21:04 -0600

    added validation before storing question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required|min:5|regex:[?]$',
+        ]);
+
+        
+
+        // $request->user()->tasks()->create([
+        //     'name' => $request->name,
+        // ]);
+
+        // return redirect('/tasks');
+    }
 }
