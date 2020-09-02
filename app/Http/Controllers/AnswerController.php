9ee33ded57c751be0f324a103c68ca242867fdce
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2020-09-02 17:35:53 -0600

    storing answers functionality added in answer controller

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Answer;
 use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required|min:5',
+            'question_id' => 'required'
+        ]);
+
+        $answer = new Answer;
+        $answer->body = $request->body;
+        $answer->question_id = $request->question_id;
+        $answer->save();
+
+        return redirect('/');
+    }
 }
