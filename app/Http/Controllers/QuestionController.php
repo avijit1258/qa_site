ddd14a93f68126b7b2e550a62eec08fbc5549b58
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2020-09-02 18:19:52 -0600

    returning all questions with their answer count done in question controller

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+use App\Answer;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -19,5 +20,23 @@ class QuestionController extends Controller
 
         return redirect('/');
     }
-    
+
+    public function allQuestionsWithTheirAnswerCount()
+    {
+        $questions = Question::order_by('created_at', 'asc')->get();
+        
+        $answer_count = array();
+
+        for($i = 0; $i < count($questions); $i++)
+        {
+            $answer_count[$questions[$i]->id] = $questions[$i]->answers()->count();
+        }
+
+        return view('home', [
+            'questions' => $questions,
+            'answer_count' => $answer_count
+        ]);
+
+    }
+
 }
